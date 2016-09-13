09d1c50144b970720de2a31cfa02c7ebca8a435c
wbazant <wbazant@ebi.ac.uk>
2016-09-13 09:42:55 +0100

    Two more generic plant icons

--- html/dist/speciesIcon.bundle.js
+++ html/dist/speciesIcon.bundle.js
@@ -424,7 +424,7 @@ webpackJsonp_name_([2],{
 			"5": ["hordeum vulgare", "hordeum vulgare subsp. vulgare"],
 			"6": ["oryza sativa", "oryza sativa japonica group"],
 			"c": "zea mays",
-			"P": ["brachypodium distachyon", "glycine max", "solanum lycopersicum", "sorghum bicolor", "vitis vinifera", "triticum aestivum"]
+			"P": ["brachypodium distachyon", "glycine max", "physcomitrella patens", "solanum lycopersicum", "solanum tuberosum", "sorghum bicolor", "vitis vinifera", "triticum aestivum"]
 		},
 		"other": {
 			"7": "anolis carolinensis",
--- src/mapping.js
+++ src/mapping.js
@@ -20,7 +20,7 @@ module.exports = {
 		"5": ["hordeum vulgare", "hordeum vulgare subsp. vulgare"],
 		"6": ["oryza sativa", "oryza sativa japonica group"],
 		"c": "zea mays",
-		"P":["brachypodium distachyon","glycine max","solanum lycopersicum","sorghum bicolor","vitis vinifera","triticum aestivum"]
+		"P":["brachypodium distachyon","glycine max","physcomitrella patens","solanum lycopersicum","solanum tuberosum","sorghum bicolor","vitis vinifera","triticum aestivum"]
 	},
 	"other": {
 		"7": "anolis carolinensis",
