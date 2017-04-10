00b160104f302f53cde930d5e981d2235317c8e4
wbazant <wbazant@ebi.ac.uk>
2017-04-10 07:52:23 +0100

    Bundles

--- html/dist/demo.bundle.js
+++ html/dist/demo.bundle.js
@@ -491,7 +491,7 @@ webpackJsonp_name_([0],{
 			"Z": "danio rerio",
 			"F": "drosophila melanogaster",
 			"W": ["caenorhabditis elegans", "schistosoma mansoni"],
-			"": "saccharomyces cerevisiae",
+			"": ["saccharomyces cerevisiae", "schizosaccharomyces pombe"],
 			"E": "tetraodon nigroviridis",
 			"f": ["xenopus (silurana) tropicalis", "xenopus tropicalis"]
 		}
--- html/dist/speciesIcon.bundle.js
+++ html/dist/speciesIcon.bundle.js
@@ -435,7 +435,7 @@ webpackJsonp_name_([2],{
 			"Z": "danio rerio",
 			"F": "drosophila melanogaster",
 			"W": ["caenorhabditis elegans", "schistosoma mansoni"],
-			"": "saccharomyces cerevisiae",
+			"": ["saccharomyces cerevisiae", "schizosaccharomyces pombe"],
 			"E": "tetraodon nigroviridis",
 			"f": ["xenopus (silurana) tropicalis", "xenopus tropicalis"]
 		}
