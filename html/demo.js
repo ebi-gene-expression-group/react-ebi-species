74a580d87016404edd8778384e4156e916bca9c7
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-07-04 14:45:39 +0100

    A better demo that uses both the component and the rendering helper method

--- html/demo.js
+++ html/demo.js
@@ -1,49 +1,32 @@
-"use strict";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-//*------------------------------------------------------------------*
+import EbiSpeciesIcon, {render} from '../src/index.js'
+import {getAllSpecies} from '../src/mapping.js'
 
-var React = require('react');
+const renderAll = (target) => {
+  const allSpecies = getAllSpecies()
 
-var ReactDOM = require('react-dom');
-
-//*------------------------------------------------------------------*
-
-var Icon = require('../src/SpeciesIcon.jsx');
-var mapping = require("../src/mapping.js");
-require("../src/ebi-visual-species.css");
-
-//*------------------------------------------------------------------*
-
-exports.render = function(mountNode) {
-  var allSpecies = [];
-  for(var kingdom in mapping){
-    if(mapping.hasOwnProperty(kingdom)){
-      for(var iconSymbol in mapping[kingdom]){
-        if (mapping[kingdom].hasOwnProperty(iconSymbol)){
-          var o = mapping[kingdom][iconSymbol];
-          if(typeof o === 'string'){
-            allSpecies.push(o);
-          } else {
-            [].push.apply(allSpecies, o);
-          }
-        }
-      }
-    }
-  }
+  const allIcons = allSpecies.map((species) =>
+    <tr key={species}>
+      <td>{species}</td>
+      <td><EbiSpeciesIcon species={species}/></td>
+    </tr>)
 
   ReactDOM.render(
-      React.createElement('div',{
-      },
-      allSpecies.map(function(species){
-        return (
-          React.createElement('div'),
-          {key:species+" parent"},
-          [
-            React.createElement('div',{style:{marginTop:'20px'}},species),
-            React.createElement(Icon, {species:species})
-          ]
-        )
-      })
-    ), mountNode
+    <div>
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Icon</th>
+        </thead>
+        <tbody>
+          {allIcons}
+        </tbody>
+      </table>
+    </div>,
+    document.getElementById(target)
   )
-};
+}
+
+export {renderAll, render}
