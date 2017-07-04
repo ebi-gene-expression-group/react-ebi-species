6d1cc5aeebf6ba491f40d60f057491ab528e223e
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-07-04 14:31:57 +0100

    Update dependencies, scripts and Webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,45 @@
-var webpack = require('webpack');
-var path = require('path');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+var webpack = require('webpack')
+var path = require('path')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: {
-        speciesIcon: './index.js',
-        demo: './html/demo.js',
-        dependencies: ['react', 'react-dom']
-    },
+  entry: {
+    ebiSpeciesIcon: './src/index.js',
+    demo: './html/demo.js',
+    dependencies: ['react', 'react-dom']
+  },
 
-    output: {
-        libraryTarget: 'var',
-        library: '[name]',
-        path: path.resolve(__dirname, 'html/dist'),
-        filename: '[name].bundle.js',
-        publicPath: 'html/dist/'
-    },
+  output: {
+    library: '[name]',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: 'html/dist/'
+  },
 
-    plugins: [
-        new CleanWebpackPlugin(['html/dist'], {verbose: true, dry: false}),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'dependencies',
-            filename: 'vendor.bundle.js',
-            minChunks: Infinity     // Explicit definition-based split. Don’t put shared modules between main and demo entries in vendor.bundle.js
-        })
-    ],
+  plugins: [
+    new CleanWebpackPlugin(['html/dist'], {verbose: true, dry: false}),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'dependencies',
+      filename: 'vendorCommons.bundle.js',
+      minChunks: Infinity     // Explicit definition-based split, see dependencies entry
+    })
+  ],
 
-    module: {
-        loaders: [
-            {test: /\.js$/, loader: 'babel', query: {presets: ['es2015']}},
-            {test: /\.jsx$/, loader: 'babel', query: {presets: ['es2015', 'react']}},
-            {test: /demo.js$/, loader: 'expose?exposed'},
-            {test: /\.css$/, loader: 'style-loader!css-loader'}
-        ]
-    }
-};
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules\//,
+        use: ['babel-loader']
+      }
+    ]
+  },
+
+  devServer: {
+    port: 9000
+  }
+}
