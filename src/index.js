953331136ba4bcb2bfce05fe302de22b48228c08
wbazant <wbazant@ebi.ac.uk>
2017-07-27 11:23:28 +0100

    Fix import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import EbiSpeciesIcon from '../src/EbiSpeciesIcon.js'
+import EbiSpeciesIcon from './EbiSpeciesIcon.js'
 
 const render = function (options, target) {
   ReactDOM.render(<EbiSpeciesIcon {...options} />, document.getElementById(target))
