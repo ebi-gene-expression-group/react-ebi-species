abf33a65f9718101e595ab3ed94ea7bb76d9a654
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2019-03-18 15:26:06 +0000

    V2.1.0 (#5)
    * Update dependencies and remove boilerplate code in tests
    
    * Add new mappings for mosquito and plasmodium species
    
    * Tidy up npm scripts

--- __test__/EbiSpeciesIcon.test.js
+++ __test__/EbiSpeciesIcon.test.js
@@ -1,13 +1,7 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import Enzyme from 'enzyme'
-import { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { shallow, mount } from 'enzyme'
 
 import { lookUpIcon, allSpecies } from '../src/mapping'
 import EbiSpeciesIcon from '../src/EbiSpeciesIcon'
-
-Enzyme.configure({ adapter: new Adapter() })
 
 const altGroupColors = {
   mammals: `brown`,
@@ -18,29 +12,27 @@ const altGroupColors = {
 describe(`EbiSpeciesIcon with classes`, () => {
   allSpecies.forEach(species => {
     test(`Icon of mapped species ${species} matches snapshot`, () => {
-      const tree = renderer.create(<EbiSpeciesIcon species={species} />).toJSON()
-      expect(tree).toMatchSnapshot()
+      expect(mount(<EbiSpeciesIcon species={species} />)).toMatchSnapshot()
     })
   })
 
   test(`Icon of unknown species matches snapshot`, () => {
-    const tree = renderer.create(<EbiSpeciesIcon species={`crocubot`} />).toJSON()
-    expect(tree).toMatchSnapshot()
+    expect(mount(<EbiSpeciesIcon species={`crocubot`} />)).toMatchSnapshot()
   })
 
   test(`uses EBI VF class names`, () => {
-    expect(shallow(<EbiSpeciesIcon species={`crocubot`} />).find(`.icon.icon-species`)).toHaveLength(1)
+    expect(shallow(<EbiSpeciesIcon species={`crocubot`} />)).toContainExactlyOneMatchingElement(`.icon.icon-species`)
   })
 
   test(`Warm-blooded species are red, violets are blue`, () => {
     const mappedMammals = allSpecies.filter((species) => lookUpIcon(species)[0] === `mammals`)
 
     mappedMammals.forEach((warmBloodedAnimal) => {
-      expect(shallow(<EbiSpeciesIcon species={warmBloodedAnimal} />).prop(`style`).color).toMatch(`red`)
+      expect(shallow(<EbiSpeciesIcon species={warmBloodedAnimal} />)).toHaveStyle(`color`, `red`)
     })
 
     mappedMammals.forEach((warmBloodedAnimal) => {
-      expect(shallow(<EbiSpeciesIcon species={warmBloodedAnimal} groupColors={altGroupColors} />).prop(`style`)).toHaveProperty(`color`, `brown`)
+      expect(shallow(<EbiSpeciesIcon species={warmBloodedAnimal} groupColors={altGroupColors} />)).toHaveStyle(`color`, `brown`)
     })
   })
 
@@ -48,11 +40,12 @@ describe(`EbiSpeciesIcon with classes`, () => {
     const mappedPlants = allSpecies.filter((species) => lookUpIcon(species)[0] === `plants`)
 
     mappedPlants.forEach(plant => {
+      // We’re happy if the colour is green-like
       expect(shallow(<EbiSpeciesIcon species={plant} />).prop(`style`).color).toMatch(`green`)
     })
 
     mappedPlants.forEach(plant => {
-      expect(shallow(<EbiSpeciesIcon species={plant} groupColors={altGroupColors} />).prop(`style`)).toHaveProperty(`color`, `orange`)
+      expect(shallow(<EbiSpeciesIcon species={plant} groupColors={altGroupColors} />)).toHaveStyle(`color`, `orange`)
     })
   })
 
@@ -60,17 +53,18 @@ describe(`EbiSpeciesIcon with classes`, () => {
     const mappedOthers = allSpecies.filter((species) => lookUpIcon(species)[0] === `other`)
 
     mappedOthers.forEach(other => {
+      // We’re happy if the colour is blue-like
       expect(shallow(<EbiSpeciesIcon species={other} />).prop(`style`).color).toMatch(`blue`)
     })
 
     mappedOthers.forEach(other => {
-      expect(shallow(<EbiSpeciesIcon species={other} groupColors={altGroupColors} />).prop(`style`)).toHaveProperty(`color`, `yellow`)
+      expect(shallow(<EbiSpeciesIcon species={other} groupColors={altGroupColors} />)).toHaveStyle(`color`, `yellow`)
     })
   })
 
   test(`admits a colour prop that overrides group colouring`, () => {
-    expect(shallow(<EbiSpeciesIcon species={`homo sapiens`} color={`yellow`}/>).prop(`style`)).toHaveProperty(`color`, `yellow`)
-    expect(shallow(<EbiSpeciesIcon species={`homo sapiens`} groupColors={altGroupColors} color={`yellow`}/>).prop(`style`)).toHaveProperty(`color`, `yellow`)
+    expect(shallow(<EbiSpeciesIcon species={`homo sapiens`} color={`yellow`}/>)).toHaveStyle(`color`, `yellow`)
+    expect(shallow(<EbiSpeciesIcon species={`homo sapiens`} groupColors={altGroupColors} color={`yellow`}/>)).toHaveStyle(`color`, `yellow`)
   })
 
 })
